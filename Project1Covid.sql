9b2c7f90826da8ba9142d6b0614fed29133798d1
Matias Scherer <63814565+matiascherer@users.noreply.github.com>
2023-11-22 22:25:39 -0300

    Update Project1Covid.sql

--- Project1Covid.sql
+++ Project1Covid.sql
@@ -30,8 +30,8 @@ ORDER BY 1,2
 SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM CovidDeaths
 WHERE Location LIKE '%zil%'
-ORDER BY 1,2
-
+ORDER BY 2 DESC
+ 
 -- Looking at Total Cases vs Population
 --Shows what percentage of population got Covid
 SELECT Location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
@@ -53,7 +53,7 @@ GROUP BY location
 ORDER BY TotalDeathsCount DESC
 
 --Showing the countries with the Highest Death Count per Population
-SELECT location, MAX((total_deaths/population))*100 AS PercentPopulationDeaths
+SELECT location, MAX(population) AS population, MAX(total_deaths) AS total_deaths, MAX((total_deaths/population))*100 AS PercentPopulationDeaths
 FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
@@ -61,6 +61,12 @@ ORDER BY PercentPopulationDeaths DESC
 
 --LET'S BREAK THINGS DOWN BY CONTINENT
 -- Showing continents with the highest Death Count per population
+SELECT continent, MAX(CAST(total_deaths AS int)) AS TotalDeathsCount
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathsCount DESC
+
 SELECT continent, MAX(CAST(total_deaths AS int)) AS TotalDeathsCount
 FROM CovidDeaths
 WHERE continent IS NOT NULL
@@ -94,6 +100,41 @@ JOIN CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 ORDER BY  2,3
 
+-- Total Cases Vs Population share per Continent over time.
+
+WITH CTE AS
+	(SELECT
+		FORMAT(Date, 'yyyy-MM') AS MonthYear,
+		Continent,
+		MAX(Population) AS Population,
+		SUM(Total_Cases) AS Total_Cases
+	FROM CovidDeaths
+	WHERE Continent IS NOT NULL AND DATE >= '2020-03-01'
+	GROUP BY FORMAT(Date, 'yyyy-MM'), Continent),
+	
+	CTE2 AS
+		(SELECT 
+			MonthYear,
+			Continent,
+			MAX(Population) AS Total_Population,
+			SUM(Total_Cases) AS Total_Cases,
+			SUM(SUM(Total_Cases)) OVER (PARTITION BY MonthYear) AS Total_Cases_All_Continents,
+			SUM(SUM(Population)) OVER (PARTITION BY MonthYear) AS Total_Population_All_Continents
+		FROM CTE
+		GROUP BY MonthYear, Continent)
+
+SELECT 
+	MonthYear,
+	Continent,
+	Total_Cases,
+	Total_Cases_All_Continents,
+	(Total_Cases/Total_Cases_All_Continents)*100 AS Total_Cases_Share,
+	Total_Population,
+	Total_Population_All_Continents,
+	(Total_Population*1.0/Total_Population_All_Continents)*100 AS Total_Population_Share
+FROM CTE2
+ORDER BY MonthYear, Continent
+
 -- CTE
 
 WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
@@ -109,7 +150,7 @@ JOIN CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 --ORDER BY  2,3
 )
-SELECT *, (RollingPeopleVaccinated/CAST(Population AS decimal))*100
+SELECT *, (RollingPeopleVaccinated/CAST(Population AS decimal))*100 PercentagePopulationVaccinated
 FROM PopvsVac
 
 --TEMP Table
@@ -137,7 +178,7 @@ WHERE dea.continent IS NOT NULL
 SELECT *, (RollingPeopleVaccinated/CAST(Population AS decimal))*100
 FROM #PercentPopulationVaccinated
 
--- Creating View to store data for later visulatizations
+-- Creating View to store data for later visualizations
 
 CREATE VIEW PercentPopulationVaccinated AS
 SELECT dea.Continent, dea.Location, dea.Date, Population, vac.New_Vaccinations, 
